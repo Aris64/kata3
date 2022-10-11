ad5e77d577eb073a0a62863a0e4aba2261862b83
Aris Vazdekis Soria <arissoriano71@gmail.com>
2022-10-11 14:25:03 +0100

    xxxxx

--- src/es/ulpgc/dacd/kata/Main.java
+++ src/es/ulpgc/dacd/kata/Main.java
@@ -1,13 +1,11 @@
 package es.ulpgc.dacd.kata;
 
 public class Main {
-<<<<<<< HEAD
+    
     public static void main(String[] args) {
         System.out.println("hola juan!!!!!!");
     }
 
-=======
->>>>>>> parent of 17c1f77 (tercer comit)
 }
 
 
