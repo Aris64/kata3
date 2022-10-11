b9e4a920b012a771ee4c689786c06d64c6398939
Aris Vazdekis Soria <arissoriano71@gmail.com>
2022-10-11 14:47:02 +0100

    xxx

--- src/es/ulpgc/dacd/kata/Main.java
+++ src/es/ulpgc/dacd/kata/Main.java
@@ -3,7 +3,7 @@ package es.ulpgc.dacd.kata;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hola juan!!!!!!");
+        System.out.println("hola juan!!!!!");
     }
 
 }
